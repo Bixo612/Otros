968d8c1d9127a242e1128258689db17baecae144
Vicente Vasquez <vicente.vasquez612@gmail.com>
2022-11-12 23:55:39 -0300

    -

---
+++ hellothere.py
@@ -0,0 +1,11 @@
+print ("Hello there")
+nombre = input("ingrese su nombre: ")
+if nombre == "Kenobi" or nombre == "kenobi" :
+    print ("General " + nombre +"!")
+else:
+    print ("Hola " + nombre + ":)")
+nombre = input("")
+
+set1 = (1,2,3,4,5)
+set2 = (1,2,3)
+print (len(set1-set2))
